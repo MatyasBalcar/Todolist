5d7949bfa036abf7ca6382ea9fd408952a4936bd
MatyasBalcar <matyasbalcar0@gmail.com>
2023-07-29 10:15:39 +0200

    Add creating projects with validations and translations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_27_134027) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_29_080317) do
+  create_table "projects", force: :cascade do |t|
+    t.string "title"
+    t.integer "position"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
