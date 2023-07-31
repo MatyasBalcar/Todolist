db0bab1786412d55ca498574e4bb28ff59cad5f4
MatyasBalcar <matyasbalcar0@gmail.com>
2023-07-31 18:56:50 +0200

    choice of task atribute added

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,13 @@
 module TasksHelper
+  def render_filtered_tasks(tasks, tasks_to_show)
+    return tasks if tasks_to_show == 'all'
+
+    if tasks_to_show == 'done'
+      tasks.select { |task| task.is_done }
+    elsif tasks_to_show == 'not_done'
+      tasks.reject { |task| task.is_done }
+    else
+      tasks
+    end
+  end
 end
