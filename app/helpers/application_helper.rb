3ea866102223cf767abe6d7692f1e358a2500b2d
MatyasBalcar <matyasbalcar0@gmail.com>
2023-08-01 21:13:49 +0200

    Add coloring to searched text , exact

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,11 @@ module ApplicationHelper
 
   # Include it in the helpers (e.g. application_helper.rb)
   include Pagy::Frontend
+  def render_highlighted_title(title, search_phrase)
+    if search_phrase.present? && title.present? && title.casecmp(search_phrase).zero?
+      content_tag(:span, title, style:"color:yellow")
+    else
+      content_tag(:span, title, style:"color:white")
+    end
+  end
 end
