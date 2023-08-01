6a47cdb2bf54fbefdcc85ff08e744b461fa4c1f6
MatyasBalcar <matyasbalcar0@gmail.com>
2023-08-01 21:52:38 +0200

    Add no tag and no proejkt to tasks screen

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,7 @@ class TasksController < ApplicationController
 
   # GET /tasks or /tasks.json
   def index
-    @tasks = Task.all.includes([:user,:tags])
+    @tasks = Task.all.includes([:user,:tags, :project])
   end
 
   # GET /tasks/1 or /tasks/1.json
