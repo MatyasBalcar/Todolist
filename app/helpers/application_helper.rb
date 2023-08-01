05c2e651b6afef576f9658597c0462f323629b25
MatyasBalcar <matyasbalcar0@gmail.com>
2023-08-01 21:36:31 +0200

    FIX made search result links work again

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,11 +4,22 @@ module ApplicationHelper
 
   # Include it in the helpers (e.g. application_helper.rb)
   include Pagy::Frontend
-  def render_highlighted_title(title, search_phrase)
+  def render_highlighted_title(combined_result, search_phrase)
+    title = combined_result.title
     if search_phrase.present? && title.present? && title.casecmp(search_phrase).zero?
       content_tag(:span, title, style:"color:yellow")
+
+      link_path = if combined_result.is_a?(Project)
+                    project_path(combined_result)
+                  elsif combined_result.is_a?(Task)
+                    task_path(combined_result)
+                  elsif combined_result.is_a?(Tag)
+                    tag_path(combined_result)
+                  end
+      link_to(raw(content_tag(:span, title, style:"color:yellow")), link_path)
     else
       content_tag(:span, title, style:"color:white")
+
     end
   end
 end
