7f6f4dabb1141d3b75fe46a5e983c2b105b31f59
MatyasBalcar <matyasbalcar0@gmail.com>
2023-08-02 17:40:02 +0200

    added basic seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+user = User.create(name: "Matyas",surname:"Balcar", email: "matyas.balcar@gmail.com",password:"123456")
+project = Project.create(title:"Projekt titulek", position: 1, user: user)
+task = Task.create(title:"Task titulek", description: "Task popisek",is_done: false, project:project, user:user)
