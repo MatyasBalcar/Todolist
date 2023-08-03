4871e8486aac69d4fb4dfb52a7aa53c47847c44a
MatyasBalcar <matyasbalcar0@gmail.com>
2023-08-03 15:39:58 +0200

    Modifiede search controller to use for_use with scope and not where

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,14 +1,20 @@
 class SearchController < ApplicationController
   def index
-    @projects_query = Project.ransack(params[:q])
-    @tasks_query = Task.ransack(params[:q])
-    @tags_query = Tag.ransack(params[:q])
+    if current_user
+      @projects_query = Project.for_user(current_user).ransack(params[:q])
+      @tasks_query = Task.for_user(current_user).ransack(params[:q])
+      @tags_query = Tag.for_user(current_user).ransack(params[:q])
+    else
+      @projects_query = Project.ransack(params[:q])
+      @tasks_query = Task.ransack(params[:q])
+      @tags_query = Tag.ransack(params[:q])
+    end
 
     @projects = @projects_query.result(distinct: true)
     @tasks = @tasks_query.result(distinct: true)
     @tags = @tags_query.result(distinct: true)
 
     # If you want to combine the results into a single array, you can do this:
-    @combined_results = @projects.to_a + @tasks.to_a+@tags.to_a
+    @combined_results = @projects.to_a + @tasks.to_a + @tags.to_a
   end
 end
