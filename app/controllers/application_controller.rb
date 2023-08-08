ba9da9b3c2cad7099b5509c09a333211c0004e41
MatyasBalcar <matyasbalcar0@gmail.com>
2023-08-08 22:01:28 +0200

    added translation for pagy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,6 @@ class ApplicationController < ActionController::Base
   # Include it in the controllers (e.g. application_controller.rb)
   include Pagy::Backend
 
-  # Include it in the helpers (e.g. application_helper.rb)
-  include Pagy::Frontend
 
   protected
 
--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -1,2 +1,3 @@
 require 'pagy/extras/support'
 require 'pagy/extras/countless'
+Pagy::I18n.load(locale: 'cs')
