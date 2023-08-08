fb951d147db7b78e56d961be81db4fe8647adb62
MatyasBalcar <matyasbalcar0@gmail.com>
2023-08-08 17:22:24 +0200

    Refactor the code

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,20 +1,10 @@
 class SearchController < ApplicationController
   def index
-    if current_user
-      @projects_query = Project.for_user(current_user).ransack(params[:q])
-      @tasks_query = Task.for_user(current_user).ransack(params[:q])
-      @tags_query = Tag.for_user(current_user).ransack(params[:q])
-    else
-      @projects_query = Project.ransack(params[:q])
-      @tasks_query = Task.ransack(params[:q])
-      @tags_query = Tag.ransack(params[:q])
-    end
 
-    @projects = @projects_query.result(distinct: true)
-    @tasks = @tasks_query.result(distinct: true)
-    @tags = @tags_query.result(distinct: true)
+    @projects = Project.for_user(current_user).ransack(params[:q]).result
+    @tasks = Task.for_user(current_user).ransack(params[:q]).result
+    @tags = Tag.for_user(current_user).ransack(params[:q]).result
 
-    # If you want to combine the results into a single array, you can do this:
     @combined_results = @projects.to_a + @tasks.to_a + @tags.to_a
   end
 end
