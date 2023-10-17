1ace43652ca616377ff3ff9c8a5fca6e96d4c1a1
matyas <matyasbalcar0@gmail.com>
2023-10-17 17:03:14 +0200

    add default avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,9 +11,13 @@ class User < ApplicationRecord
   has_many :tasks , dependent: :destroy
   has_many :tags, dependent: :destroy
   has_one_attached :file
-  validates :file, presence: true
-  def avatar_thumbnail(x)
-    file.variant(resize: (x.to_s+'x'+x.to_s+'!')).processed
+  #validates :file, presence: true
+  def avatar_thumbnail(x,user)
+    if user.file.attached?
+      file.variant(resize: (x.to_s+'x'+x.to_s+'!')).processed
+    else
+      '/default_avatar.png'
   end
 
-end
+  end
+  end
